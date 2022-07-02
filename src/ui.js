90a8072e3e86ce823200be2e907cab9c2f12d308
Melk12345 <105999204+Melk12345@users.noreply.github.com>
2022-07-02 16:06:54 -0700

    Add files via upload

---
+++ src/ui.js
@@ -0,0 +1,25 @@
+const farmMenuContainerElement = document.getElementById("farm-container");
+const cropsMenuContainerElement = document.getElementById("crops-container");
+const settingsMenuContainerElement = document.getElementById("settings-container");
+
+let activeMenu = farmMenuContainerElement;
+cropsMenuContainerElement.style.display = "none";
+settingsMenuContainerElement.style.display = "none";
+
+const farmMenuButtonElement = document.getElementById("farm-menu-button");
+const cropsMenuButtonElement = document.getElementById("crops-menu-button");
+const settingsMenuButtonElement = document.getElementById("settings-menu-button");
+
+let activeMenuButton = farmMenuButtonElement;
+farmMenuButtonElement.style.borderColor = 'Orange';
+cropsMenuButtonElement.style.borderColor = 'Black';
+settingsMenuButtonElement.style.borderColor = 'Black'; 
+
+function openMenu(clickedMenu, clickedMenuButton) {
+    activeMenu.style.display = "none";
+    activeMenuButton.style.borderColor = 'Black';
+    activeMenu = clickedMenu;
+    activeMenuButton = clickedMenuButton;
+    activeMenuButton.style.borderColor = 'Orange';
+    activeMenu.style.display = "block";
+}
